0b52795a0c7b4e9752b41281a078b5bd73d23ab9
dev001hajipro <dev001hajipro@gmail.com>
2018-02-09 18:00:26 +0900

    add bgm

---
+++ Assets/Scripts/BGMScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour
+{
+
+	public static BGMScript Instance {
+		get;
+		private set;
+	}
+
+	void Awake ()
+	{
+		if (Instance != null && Instance != this) {
+			Debug.Log ("Destroy BGMScript parent.1!!!!");
+			DestroyObject (this.gameObject);
+		} else { // Instance is NULL.
+			Instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		}
+	}
+}
--- MEMO.md
+++ MEMO.md
@@ -10,3 +10,6 @@ https://github.com/github/gitignore/blob/master/Unity.gitignore
 ## Portrait
 Gameタブで3:2 Portrait(2:3)を指定し
 Build SettingsでAndroidにswitch platformすると、Androidにデプロイしたらポートレートになった。
+
+## シーンが変わってもBGMを鳴らし続ける。
+https://answers.unity.com/questions/11314/audio-or-music-to-continue-playing-between-scene-c.html
